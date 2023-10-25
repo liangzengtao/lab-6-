00d15e74d3960a270db8f61805a0219ce171e15d
liangzengtao <148926486+liangzengtao@users.noreply.github.com>
2023-10-25 17:54:42 -0400

    Update Lab 6 .py

--- Lab 6 .py
+++ Lab 6 .py
@@ -2,15 +2,30 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def encode(password):
+    result=""
+    for i in range(0,len(password)):
+        if (int(password[i])+3>=10):
+            result+=str((int(password[i])+3)%10)
+        else:
+            result+=str(int(password[i])+3)
+    return result
+def main():
+    user_input=-1
+    while user_input!=3:
+        print_menu()
+        user_input = int(input('Please enter an option: '))
+        if user_input==1:
+            user_password = input("Please enter your password to encode: ")
+            encode_password = (encode(user_password))
+            print("Your password has been encoded and stored!")
+            user_input=int(input('Please enter an option: '))
+        if user_input==2:
+            
